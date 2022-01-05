d69ebe852776ef7e504336f20a62c5430a416c20
Paulmota <64449492+Paulmota@users.noreply.github.com>
2022-01-05 12:09:27 -0600

    Ejemplos de python

---
+++ Ejercicios.py
@@ -0,0 +1,62 @@
+# Ejercicio Hola mundo
+
+#print("Hola mundo")
+
+
+# Ejercicio suma de variables numericas
+
+"""valor1=int(input("Ingresa el primer valor: "))
+valor2=int(input("Ingresa el segundo valor: "))
+resultado=valor1+valor2
+print("El resultado de la suma es: "+str(resultado))"""
+
+# Ejercicio de mostrar el precio del IVA
+
+"""IVA=0.10
+producto=100
+precio_iva = producto*IVA
+resultado=producto+precio_iva
+print("El valor del IVA es: "+str(precio_iva))
+print("El precio final de la compra es de: "+str(resultado))"""
+
+# Ejercicio cual es el numero mayor
+
+"""valor1=int(input("Ingresa el primer valor: "))
+valor2=int(input("Ingresa el segundo valor: "))
+
+if valor1>valor2:
+    print("El valor 1 es mayor: ",valor1)
+else:
+    print("El segundo valor es mayor: ", valor2)"""
+
+# Ejercicio variable numerica con rango para mandar msj
+
+"""valor=int(input("Ingresa un numero: "))
+
+if valor<=10:
+    print("Este numero esta entre 0 y 10")
+else:
+    if valor<20:
+        print("Este numero es mayor a 10 pero menor a 20")
+    else:
+        if valor<=30:
+            print("Este numero es mayor a 20 pero menor a 30")
+        else:
+            print("Este numero es mayor a 30")"""
+
+# Mostar con un WHILE los numeros del 1 al 100
+
+"""x=1
+
+while x<=100:
+    print(x)
+    x+=1
+print("Fin del programa")"""
+
+# Mostar con un FOR los numeros del 1 al 100
+
+"""for x in range (100):
+    x+=1
+print("Fin de programa")"""
+
+
